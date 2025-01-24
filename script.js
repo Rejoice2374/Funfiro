042fa152ef3c47388e6753e56e68c4938f124529
Rejoice2374 <adeboyerejoice2374@gmail.com>
2025-01-24 15:45:14 +0100

    more

---
+++ script.js
@@ -0,0 +1,10 @@
+const itemBox = document.querySelector(".item");
+const popUp = document.querySelector(".popup");
+
+itemBox.addEventListener("mouseover", function () {
+  popUp.style.display = "block";
+});
+
+itemBox.addEventListener("mouseout", function () {
+  popUp.style.display = "none";
+});
