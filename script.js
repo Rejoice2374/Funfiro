5c7d09a06d893f9d923309ec1092e7828f297e50
Rejoice2374 <adeboyerejoice2374@gmail.com>
2025-02-02 22:18:03 +0100

    hover

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const itemBox = document.querySelector(".item");
-const popUp = document.querySelector(".popup");
+// const itemBox = document.querySelector(".item");
+// const popUp = document.querySelector(".popup");
 
-itemBox.addEventListener("mouseover", function () {
-  popUp.style.display = "block";
-});
+// itemBox.addEventListener("mouseover", function () {
+//   popUp.style.display = "block";
+// });
 
-itemBox.addEventListener("mouseout", function () {
-  popUp.style.display = "none";
-});
+// itemBox.addEventListener("mouseout", function () {
+//   popUp.style.display = "none";
+// });
